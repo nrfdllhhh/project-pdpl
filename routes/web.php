c7ac79e88b69a00533259385318a5f0285799ff4
HilmiKamil <hilmihelios@gmail.com>
2024-12-24 05:58:18 +0700

    Layout, Login and Register UI

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('admin/dashboard');
+})->name('admin');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+
+Route::get('/scheduleTask', function () {
+    return view('admin/scheduleTask');
+})->name('scheduleTask');
